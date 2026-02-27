bdf43a4d2b76f0fa77fdbe21636abf5c99e18139
NandyAkash <nandyak81@gmail.com>
2026-02-27 14:15:59 +0600

    Add basic error handling for division

--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
 import datetime
-from utils import add, subtract, multiply
+from utils import add, subtract, multiply, divide
 
 print("Name: Akash Nandy")
 print("Today's Date:", datetime.date.today())
 
-print("Addition:", add(10, 5))
-print("Subtraction:", subtract(10, 5))
-print("Multiplition:", multiply(10, 5))
+print("Addition:", add(10, 20))
+print("Subtraction:", subtract(10, 4))
+print("Multiplition:", multiply(10, 10))
+print("Division:", divide(10, 0))
